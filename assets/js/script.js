1eac3373ffdf740a55e5ff89eeb297638cc602e5
femc31 <correopruebafmunoz@gmail.com>
2022-02-17 20:45:14 -0300

    tipos de mensajes que se ejecutan con JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
-console.log("probando si esta agregado el archivo js");
-alert();
-prompt();
-confirm();
+//mensajes por consola
+console.log('mensaje enviado por consola');
+console.warn('mensaje de alerta');
+console.error('esto es un error como si fuera real');
+
+//mensajes emergentes
+alert('mensaje emergente informativo');
+prompt('mensaje emergente para solicitar datos');
+confirm('mensaje emergente para solicitar confirmacion');
+
+//mensajes por el navegador
+document.write('<h1>Muestra lo que desea por el navegador, por ejemplo: "esto es un titulo"</h1>');
 /**de momento, el document es una variable especial */
-document.write();
